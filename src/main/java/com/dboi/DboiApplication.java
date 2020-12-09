2916cc4b9abe0a01e13a2bcdc7ba1551126923dd
jigarshah13 <jigar.shah13@gmail.com>
2020-12-09 16:03:54 +0530

    Initial DBOI Project Commit

---
+++ src/main/java/com/dboi/DboiApplication.java
@@ -0,0 +1,13 @@
+package com.dboi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DboiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DboiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dboi/DboiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dboi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DboiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
